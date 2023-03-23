728db639f40b1faed7d5ef569e2d3c781444779e
wov <wovfeng@gmail.com>
2023-03-23 14:20:50 +0800

    add a font

--- spiderweb/pages/_app.js
+++ spiderweb/pages/_app.js
@@ -1,5 +1,12 @@
 import '@/styles/globals.css'
+import localFont from 'next/font/local'
+
+// Font files can be colocated inside of `pages`
+const GeoSlab703MdBT = localFont({ src: './GeoSlab703MdBT.ttf' })
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return  <main className={GeoSlab703MdBT.className}>
+            <Component {...pageProps} />
+           </main>
 }
